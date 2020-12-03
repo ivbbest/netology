6404a9e51de4564df3ffc2363053d3f543a585b4
Irkhin Vladimir <ivb1989@yandex.ru>
2020-12-03 12:54:44 +0300

    Create 2 def: analysis json and print top 10 words in text, analysis xml and print top10 words in text.

--- XML и JSON/json-xml-basic.py
+++ XML и JSON/json-xml-basic.py
@@ -1,39 +1,78 @@
 import json
-
-with open('newsafr.json', encoding='utf-8') as f:
-    json_data = json.load(f)
-
-list_words = list()
-set_words = set()
-tmp_str = ''
+import xml.etree.ElementTree as ET
 
 
-len_json = len(json_data["rss"]["channel"]["items"])
+def analysis_json(file):
+    """
+    Программа, которая будет выводить топ 10 самых часто встречающихся в новостях слов длиннее
+    6 символов для файла json.
+    """
 
-#создание большой строки со всеми данными из дескрипшен
-for i in range(len_json):
-    my_words = (json_data["rss"]["channel"]["items"][i]["description"]).lower()
-    tmp_str += ''.join(my_words)
+    with open(file, encoding='utf-8') as f:
+        json_data = json.load(f)
 
-#список с уникальными словами
-list_words = list(tmp_str.split())
-poisk = dict()
+    tmp_str = ''
+    len_json = len(json_data["rss"]["channel"]["items"])
 
-for elem in list_words:
-    len_word = len(elem)
+    #создание большой строки со всеми данными из дескрипшен
+    for i in range(len_json):
+        my_words = (json_data["rss"]["channel"]["items"][i]["description"]).lower()
+        tmp_str += ''.join(my_words)
 
-    if len_word > 6:
+    #список с уникальными словами
+    list_words = list(tmp_str.split())
+    poisk = dict()
 
-        #print(elem)
-        if elem in poisk:
-            poisk[elem] += 1
-        else:
-            poisk[elem] = 1
-print(poisk)
-#сортировка словаря: выходе получается список из кортежей
-list_d = list(poisk.items())
-list_d.sort(key=lambda i: i[1])
+    for elem in list_words:
+        len_word = len(elem)
 
-#печать первых 10 слов
-for el in list_d[0:10]:
-    print(el)
+        if len_word > 6:
+
+            if elem in poisk:
+                poisk[elem] += 1
+            else:
+                poisk[elem] = 1
+
+    #сортировка словаря и печать 10 популярных слов
+    popular_words = sorted(poisk.items(), key=lambda x: x[1], reverse=True)[:10]
+    print(popular_words)
+
+def analysis_xml(file):
+    """
+    Программа, которая будет выводить топ 10 самых часто встречающихся в новостях слов длиннее
+    6 символов для файла xml.
+    """
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(file, parser)
+    root = tree.getroot()
+    xml_text = ''
+    news_xml = root.findall("channel/item")
+
+    for news in news_xml:
+        descr = news.find("description")
+        xml_text += ''.join(descr.text.lower())
+
+    #список с уникальными словами
+    list_words = list(xml_text.split())
+    poisk = dict()
+
+    for elem in list_words:
+        len_word = len(elem)
+
+        if len_word > 6:
+
+            if elem in poisk:
+                poisk[elem] += 1
+            else:
+                poisk[elem] = 1
+
+    #сортировка словаря и печать 10 популярных слов
+    popular_words = sorted(poisk.items(), key=lambda x: x[1], reverse=True)[:10]
+    print(popular_words)
+
+
+file_json = 'newsafr.json'
+file_xml = 'newsafr.xml'
+
+analysis_json(file_json)
+analysis_xml(file_xml)
