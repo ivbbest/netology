4f4e8586dae1996292182069b28a79dbda91f309
Irkhin Vladimir <ivb1989@yandex.ru>
2020-12-11 14:47:54 +0300

    Finish yandex-disk task. Create 2 def and class

--- yandex-disk.py
+++ yandex-disk.py
@@ -1,51 +1,32 @@
 import requests
-import os
-from pprint import pprint
-
-HEADERS = {"Authorization": "OAuth mytoken"}
-#
-# class YaUploader:
-#     def __init__(self, token):
-#         self.token = token
-#
-#     def upload(self, file_path: str):
-#         """Метод загруджает файл file_path на яндекс диск"""
-#         resp = requests.get("https://cloud-api.yandex.net/v1/disk/resources/upload", params={"path" : "/"})
-#         print(resp)
-#         return 'Вернуть ответ об успешной загрузке'
-#
-#
-# if __name__ == '__main__':
-#     token = 'AgAAAAAZhU-gAADLWwQPbEqXgkAQt0catuijfnk'
-#     uploader = YaUploader(token)
-#     result = uploader.upload('c:\my_folder\file.txt')
 
 
-# resp = requests.get(
-#     "https://cloud-api.yandex.net/v1/disk/resources/",
-#     params={"path": "/"},
-#     headers=HEADERS,
-# )
-file = 'file.txt'
+class YaUploader:
+    def __init__(self, token):
+        self.token = token
 
-if os.path.isfile(file):
-    with open(file, encoding='UTF-8') as f:
+    def upload(self, file: str):
+        """Метод загруджает файл file_path на яндекс диск"""
+
         resp = requests.get(
-            "https://cloud-api.yandex.net/v1/disk/resources/upload",
-            headers=HEADERS,
-            params={"path": file},
+        "https://cloud-api.yandex.net/v1/disk/resources/upload",
+        headers=self.token,
+        params={"path": file}
         )
 
-        data = resp.json()
-        # pprint(data)
-        print(data['href'])
+        put_url = resp.json().get('href')
+        print(put_url)
 
-        upload_file = requests.put(
-            data['href'],
-            params={"path": file},
-            headers=HEADERS,
+        files = {'file': open(file, 'rb')}
 
-        )
+        response = requests.put(put_url, files=files, headers=self.token)
+        print(response)
+        return 'Вернуть ответ об успешной загрузке'
 
 
-        print(upload_file.raise_for_status())
+if __name__ == '__main__':
+    token = {"Authorization": "OAuth AgAAAAAZhU-gAADLWwQPbEqXgkAQt0catuijfnk"}
+    uploader = YaUploader(token)
+    result = uploader.upload('file.txt')
+
+
