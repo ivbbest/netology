ec794665f9eddd8d6f4d1a642179ab8c75e8d649
Irkhin Vladimir <ivb1989@yandex.ru>
2021-02-23 19:00:16 +0300

    Смог сделать нормальный список из существующего по задаче. Не получается уникализировать информацию, если одинаковая фамилия

--- Regexp/regexp.py
+++ Regexp/regexp.py
@@ -2,29 +2,105 @@ from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
 import re
-
+from itertools import chain
 
 with open("phonebook_raw.csv", encoding='UTF-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
 
-pprint(str(contacts_list[1][0]).split(' '))
-pprint(contacts_list)
+# pprint(str(contacts_list[1][0]).split(' '))
+# pprint(contacts_list)
 
 
 new_list = list()
-new_list.append(contacts_list[0])
-len_contacts_list = len(contacts_list)
+tmp_list = list()
+contacts_list_new = list()
 print('**********')
+
+for i in range(len(contacts_list)):
+    for j in range(len(contacts_list[i])):
+        if (0 <= j <= 1) and (' ' in contacts_list[i][j]):
+            tmp = str(contacts_list[i][j]).split(' ')
+            for el in tmp:
+                tmp_list.append(el)
+
+        elif contacts_list[i][j] != '':
+            tmp_list.append(contacts_list[i][j])
+
+    new_list.append(tmp_list)
+    tmp_list = []
+
+# pprint(new_list)
+#####################################################################
+# ВАРИАНТ 1 как хотел уникализировать информацию в списке
+
+# contacts_list_new = (list(set(chain.from_iterable(new_list))))
+
+#####################################################################
+
+#ВАРИАНТ 2 как хотел уникализировать информацию в списке
+
+# my_dict = {}
+#
+# for my_list in new_list:
+#     j = my_list[0]
+#     print(j)
+#     if j in my_dict.keys():
+#         tmp = my_dict.get(j)
+#         my_dict[j] = set(tmp) & set([my_list[el] for el in range(len(my_list))])
+#
+#     else:
+#         my_dict[j] = set([my_list[el] for el in range(len(my_list))])
+#
+#
+# pprint(my_dict)
+######################################################################
+
+# for k, v in my_dict.items():
+#     contacts_list_new.append(k)
+#     contacts_list_new.append(list(v))
+# #
+# pprint(contacts_list_new)
+
+# for i, row in enumerate(new_list):
+#     tmp_list = []
+#     for j, elem in enumerate(row):
+#         print(row)
+#         if elem in [el for a_list in contacts_list_new for el in a_list] and j == 0:
+#             # for a_list in contacts_list:
+#             print(True)
+#
+#     contacts_list_new.append(row)
+
+# pprint(contacts_list)
+
+# trans = zip(*new_list)
+# for el in trans:
+#     print(el)
+
+# for i, row in enumerate(new_list):
+#     tmp_list = []
+#     for j, elem in enumerate(row):
+#         if elem in [el for a_list in contacts_list for el in a_list] and j == 0:
+#             pos = contacts_list.index(el for a_list in contacts_list for el in a_list)
+#             print(pos)
+#
+#     contacts_list.append(row)
+
+# pprint(contacts_list)
+# pattern_telephone = re.compile(r"(\+7|8)?[\s]*\(?(\d+)\)?\s*(\d+)[\s-]*(\d+)[\s-]*(\d+)*(\s?\(?\w{3}\.\s?\d{4}\)?)*")
+# text2 = pattern.sub(r"+7-\2-\3-\4-\5", text)
+
 # for elem in contacts_list:
 #     for i in range(len(elem)):
 #
 #         if (i == 0) and (' ' in elem[i]):
 #             tmp = str(elem[i]).split(' ')
-#             # print(tmp)
-#             # print(type(tmp))
-#             # print(type(elem))
-#             # print(type(elem[i]))
+#             print(tmp)
+#             print(
+#             type(tmp))
+#             print(type(elem))
+#             print(type(elem[i]))
 #             new_list.extend(tmp)
 #
 #         else:
@@ -33,22 +109,7 @@ print('**********')
 # print('********************')
 # pprint(new_list)
 
-    # print(elem[])
-for i in range(1, len_contacts_list):
-    for j in range(len(contacts_list[i])):
-        if (0 <= j <= 1) and (' ' in contacts_list[i][j]):
-            tmp = str(contacts_list[i][j]).split(' ')
-            new_list.append(tmp)
-
-        elif contacts_list[i][j] == '':
-            continue
-        else:
-            new_list.append(contacts_list[i][j])
-
-
-pprint(new_list)
-# pattern_telephone = re.compile(r"(\+7|8)?[\s]*\(?(\d+)\)?\s*(\d+)[\s-]*(\d+)[\s-]*(\d+)*(\s?\(?\w{3}\.\s?\d{4}\)?)*")
-# text2 = pattern.sub(r"+7-\2-\3-\4-\5", text)
+# print(elem[])
 
 # TODO 1: выполните пункты 1-3 ДЗ
 # ваш код
--- Regexp/test.py
+++
@@ -1,5 +0,0 @@
-c = [[1, 2], [3, 4], [5, 6]]
-# Давайте преобразуем эту матрицу в одномерный список
-import itertools as it
-newlist = list(it.chain.from_iterable(c))
-print(newlist)
