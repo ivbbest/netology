b99e684161efb71ac1ad9954142c1c24c032fe68
Irkhin Vladimir <ivb1989@yandex.ru>
2021-03-05 16:27:58 +0300

    Завершил задачу с API VK. Все работает верно.

--- VK/vk-api.py
+++ VK/vk-api.py
@@ -13,7 +13,7 @@
 # токен и версия api являются обязательными параметрами во всех запросах к vk
 
 import requests
-
+from pprint import pprint
 
 token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
 
@@ -21,39 +21,42 @@ token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c
 class VkUser:
     url = 'https://api.vk.com/method/'
 
-    def __init__(self, token, version):
+    def __init__(self, token, version, user_id=''):
         self.token = token
         self.version = version
         self.params = {
             'access_token': self.token,
             'v': self.version
         }
-        self.owner_id = requests.get(self.url+'users.get', self.params).json()['response'][0]['id']
-        self.friends = []
+        # self.owner_id = requests.get(self.url+'users.get', self.params).json()['response'][0]['id']
+        self.user_id = user_id
 
     def __and__(self, other):
-        return self.friends_get() & other
+        '''Найти общих друзей'''
+        resp1 = self.get_friends()
+        resp2 = other.get_friends()
+        common = []
+        for item in resp1['response']['items']:
+            if item in resp2['response']['items']:
+                common.append(item)
+        return common
 
     # получим своих подписчиков при помощи [users.getFollowers](https://vk.com/dev/users.getFollowers)
-    def get_followers(self, user_id=None):
-        if user_id is None:
-            user_id = self.owner_id
+    def get_followers(self):
         followers_url = self.url + 'users.getFollowers'
         followers_params = {
             'count': 1000,
-            'user_id': user_id
+            'user_id': self.user_id
         }
         res = requests.get(followers_url, params={**self.params, **followers_params})
         return res.json()
 
     # Получим базовую информацию о пользователе при помощи [users.get](https://vk.com/dev/users.get)
-    def get_groups(self, user_id=None):
-        if user_id is None:
-            user_id = self.owner_id
+    def get_groups(self):
         groups_url = self.url + 'groups.get'
         groups_params = {
             'count': 1000,
-            'user_id': user_id,
+            'user_id': self.user_id,
             'extended': 1,
             'fields': 'members_count'
         }
@@ -61,71 +64,64 @@ class VkUser:
         return res.json()
 
     # Получим базовую информацию о пользователе при помощи [users.get](https://vk.com/dev/users.get)
-    def users_get(self, user_id=None):
-        if user_id is None:
-            user_id = self.owner_id
+    def users_get(self):
         users_url = self.url + 'users.get'
         user_params = {
             'count': 1000,
-            'user_id': user_id,
+            'user_id': self.user_id,
         }
         res = requests.get(users_url, params={**self.params, **user_params})
         return res.json()
 
-
     # Получим базовую информацию о пользователе при помощи [friends.get](https://vk.com/dev/friends.get)
-    def friends_get(self, user_id=None):
-        if user_id is None:
-            user_id = self.owner_id
+    def get_friends(self):
+        '''Получить список друзей'''
         users_url = self.url + 'friends.get'
         user_params = {
-            'user_id': user_id,
+            'user_id': self.user_id,
         }
         res = requests.get(users_url, params={**self.params, **user_params})
-        return set(res.json()['response']['items'])
+        return res.json()
 
-    #поиск общих друзей из 2 пользователей
-    # def mutual_friends(self, user_id1=None, user_id2=None):
-    #     if user_id1 is None:
-    #         user_id1 = self.owner_id
-    #
-    #     if user_id2 is None:
-    #         user_id2 = self.owner_id
-    #
-    #     if user_id1 == user_id2:
-    #         return 'Вы ввели некорректные данные пользователей, у которых требуется найти общих друзей!!!\n'
-    #
-    #     else:
-    #         self.friends = list(set(self.friends_get(user_id1)) & \
-    #                set(self.friends_get(user_id2)))
-    #
-    #         return self.friends
-    #
-    #
-    # def get_common_friend(self):
-    #        result = ''
-    #        for friend in self.friends:
-    #            if str(friend).isdigit():
-    #                result += f'https://vk.com/id{friend}\n'
-    #
-    #            else:
-    #                result += f'https://vk.com/{friend}\n'
-    #
-    #        return result
+    def get_status(self):
+        '''Получить статус пользователя'''
+        users_url = self.url + 'status.get'
+        user_params = {
+            'user_id': self.user_id,
+        }
+        res = requests.get(users_url, params={**self.params, **user_params})
+        return res.json()
 
-    def __str__(self):
-       pass
+    def set_status(self, text):
+        '''Установка статуса пользователя'''
+        users_url = self.url + 'status.get'
+        user_params = {
+            'user_id': self.user_id,
+            'text': text
+        }
+        res = requests.get(users_url, params={**self.params, **user_params})
 
-    # __repr__ = __str__
+        return res.json()
+
+    def __repr__(self):
+        return 'https://vk.com/id' + str(self.user_id)
+
+
+def url_from_id(user_id):
+    """Сделать id пользователя готовым урлом"""
+    if str(user_id).isdigit():
+        url = f'https://vk.com/id{user_id}'
+    else:
+        url = f'https://vk.com/{user_id}'
+
+    return url
 
 
 if __name__ == '__main__':
-    vk_client = VkUser(token, '5.126')
-    #print(vk_client.get_groups())
-    #print(vk_client.friends_get())
-    # common_friends = vk_client.mutual_friends(15871719, 138611543)
-    usr1 = vk_client.friends_get(15871719)
-    usr2 = vk_client.friends_get(138611543)
-    mult = usr1 & usr2
-    for el in mult:
-        print(f'https://vk.com/id{el}/')
+    usr1 = VkUser(token, '5.126', '15871719')
+    # pprint(usr1.friends_get())
+    usr2 = VkUser(token, '5.126', '138611543')
+    # pprint(usr2.friends_get())
+    # print(usr1 & usr2)
+    for elem in usr1 & usr2:
+        print(url_from_id(elem))
