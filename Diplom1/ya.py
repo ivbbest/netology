713efb725b29ed32c9feb4b2186809068599b57c
Irkhin Vladimir <ivb1989@yandex.ru>
2021-03-06 17:07:48 +0300

    Добавил считывание картинок из определенной папки, а затем залив их на ЯНдекс диск в нужную папку

--- Diplom1/ya.py
+++ Diplom1/ya.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 
 class YaUploader:
@@ -11,20 +12,24 @@ class YaUploader:
         response = requests.put('https://cloud-api.yandex.net/v1/disk/resources',
                                 params={"path": my_dir},
                                 headers=self.token)
+
         return response.status_code
 
-    def upload(self, file: str):
+    def upload(self, file: str, directory='/'):
         """Метод загружает файл file на яндекс диск"""
+
+        if directory != '/':
+            directory = directory + '/'
 
         resp = requests.get(
         "https://cloud-api.yandex.net/v1/disk/resources/upload",
         headers=self.token,
-        params={"path": f'v/{file}'}
+        params={"path": f'{directory}{file}'}
         )
 
         put_url = resp.json().get('href')
         print(put_url)
-        files = {'file': open(file, 'rb')}
+        files = {'file': open(f'images/{file}', 'rb')}
 
         response = requests.put(put_url, files=files, headers=self.token)
         print(response)
@@ -33,7 +38,12 @@ class YaUploader:
 
 if __name__ == '__main__':
     token = {"Authorization": "OAuth AgAEA7qiJubnAADLW5Yd-GCDLkW4iWW1iQbIkCk"}
+    directory = 'vk'
     uploader = YaUploader(token)
-    # result = uploader.create_dir('v')
-    result2 = uploader.upload('file.txt')
-    print(result2)
+    status_dir = uploader.create_dir(directory)
+    print(status_dir)
+    if 200 <= status_dir <= 201 or status_dir == 409:
+        images = os.listdir('images')
+        print(images)
+        for image in images:
+            uploader.upload(image, directory)
